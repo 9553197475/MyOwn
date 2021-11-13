2d3c84b890c3adc32cbdd04b348c74f535662c7e
ravib <ravib@LAPTOP-BQ15E4CG>
2021-11-13 15:48:21 +0530

    First Commit

---
+++ fgi/src/main/java/fgi/main/java/FGIBGLauncher.java
@@ -0,0 +1,11 @@
+package fgi.main.java;
+
+public class FGIBGLauncher {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sample Class");
+
+	}
+
+}
